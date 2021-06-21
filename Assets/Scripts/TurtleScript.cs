5450b5914d42034cafca8b9eb55c10ed75746bb0
Bianca Ritorto <bianritorto@fb.com>
2021-06-21 19:49:02 -0300

    Characters moving

---
+++ Assets/Scripts/TurtleScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class TurtleScript : MonoBehaviour
+{
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
